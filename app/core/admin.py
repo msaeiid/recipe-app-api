f9eef533de7ce6429e8a78a49df221ab3fc85cfa
msaeiid <mskarbaschian@gmail.com>
2024-09-07 15:17:17 +0330

    Support creating users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,50 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Define thr admin pages for users."""
+    ordering = ['id',]
+    list_display = ['email', 'name',]
+    # because in our model we don't have username field instead we used email,
+    # we should define fieldsets with the fields we defined in our model
+    # field set categorize each section in detail view of the user model
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+
+    )
+    readonly_fields = ['last_login',]
+    # fields which are used when creating a user
+    add_fieldsets = [
+        (None, {
+            'classes': ('wide',),
+            'fields': [
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ]
+        }),
+    ]
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # password=None because we can create unusable user for some use cases like **test**
+        if not email:
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -23,9 +25,12 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        user = self.create_user(email=email, password=password,
-                                extra_fields=extra_fields)
+        """Create and return a new superuser."""
+        user = self.create_user(email,
+                                password,
+                                **extra_fields)
         user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
