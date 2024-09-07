74f53411e3afaa92b640ad7054838d3223e4e01c
msaeiid <mskarbaschian@gmail.com>
2024-09-07 11:44:40 +0330

    Normalize email addresses

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # password=None because we can create unusable user for some use cases like **test**
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         # If you have multiple databases, its a best practice to do so
