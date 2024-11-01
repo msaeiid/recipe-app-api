3452ef9f6533ae62c4e4246d35db85b983422dbd
Mohammad Saeid Karbaschian <mskarbaschian@gmail.com>
2024-11-01 16:41:40 +0330

    Final

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,10 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+# This decorator replaces the check method in the Command
+# class (inside the wait_for_db custom management
+# command) with a mock during the test.
+
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
@@ -16,13 +20,18 @@ class CommandTests(SimpleTestCase):
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
-
+        # This line simulates that the database check
+        # (check method) immediately returns True,
+        # meaning the database is ready.
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
 
+    # This decorator mocks the time.sleep function
+    # to prevent the test from actually waiting
+    # during the retries.
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
